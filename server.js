5b106b9f432a6f786bad8a531c33c4d619f59dba
Muhammad Abu Bakar <128064938+abubakar2029@users.noreply.github.com>
2024-02-01 05:10:41 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 /* ----uuid-generator-------- */
 const { v4: uuidv1 } = require("uuid");
@@ -81,7 +81,9 @@ app.post("/quiz-submit", (req, res) => {
 });
 
 app.use(express.static("./build"));
-
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build","index.html"));
+});
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
